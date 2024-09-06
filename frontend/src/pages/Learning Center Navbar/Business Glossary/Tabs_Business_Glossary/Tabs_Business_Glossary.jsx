d43deca75695d46b03ee5acb572fc292c8508cd7
itzkbp <hi@kailashpoudel.com.np>
2024-09-06 20:23:07 +0545

    complete Business Glossarry Page

--- frontend/src/pages/Learning Center Navbar/Business Glossary/Tabs_Business_Glossary/Tabs_Business_Glossary.jsx
+++ frontend/src/pages/Learning Center Navbar/Business Glossary/Tabs_Business_Glossary/Tabs_Business_Glossary.jsx
@@ -3,6 +3,16 @@ import './Tabs_Business_Glossary.css';
 
 import Content_A from './Content_Business_Glossary/Content_A';
 import Content_B from './Content_Business_Glossary/Content_B';
+import Content_C from './Content_Business_Glossary/Content_C';
+import Content_D from './Content_Business_Glossary/Content_D';
+import Content_EG from './Content_Business_Glossary/Content_EG';
+import Content_HL from './Content_Business_Glossary/Content_HL';
+import Content_M from './Content_Business_Glossary/Content_M';
+import Content_NO from './Content_Business_Glossary/Content_NO';
+import Content_P from './Content_Business_Glossary/Content_P';
+import Content_QR from './Content_Business_Glossary/Content_QR';
+import Content_S from './Content_Business_Glossary/Content_S';
+import Content_TZ from './Content_Business_Glossary/Content_TZ';
 
 const Tabs_Business_Glossary = () => {
     const [activeTab, setActiveTab] = useState('A');
@@ -11,16 +21,16 @@ const Tabs_Business_Glossary = () => {
     const tabs = [
         { name: 'A', content: <Content_A /> },
         { name: 'B', content: <Content_B /> },
-        { name: 'C', content: <p>Content for tab C</p> },
-        { name: 'D', content: <p>Content for tab D</p> },
-        { name: 'E-G', content: <p>Content for tabs E-G</p> },
-        { name: 'H-L', content: <p>Content for tabs H-L</p> },
-        { name: 'M', content: <p>Content for tab M</p> },
-        { name: 'N-O', content: <p>Content for tabs N-O</p> },
-        { name: 'P', content: <p>Content for tab P</p> },
-        { name: 'Q-R', content: <p>Content for tabs Q-R</p> },
-        { name: 'S', content: <p>Content for tab S</p> },
-        { name: 'T-Z', content: <p>Content for tabs T-Z</p> },
+        { name: 'C', content: <Content_C /> },
+        { name: 'D', content: <Content_D /> },
+        { name: 'E-G', content: <Content_EG /> },
+        { name: 'H-L', content: <Content_HL /> },
+        { name: 'M', content: <Content_M /> },
+        { name: 'N-O', content: <Content_NO /> },
+        { name: 'P', content: <Content_P /> },
+        { name: 'Q-R', content: <Content_QR /> },
+        { name: 'S', content: <Content_S /> },
+        { name: 'T-Z', content: <Content_TZ /> },
     ];
 
     const handleTabClick = (tab) => {
